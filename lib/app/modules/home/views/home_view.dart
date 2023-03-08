c6fd179ea12c4864431caeda190676aac0339475
Mohamad Fajar Hafidh <mfajarhafidh9@gmail.com>
2023-03-08 13:15:16 +0700

    fix: horizontal padding on list article

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -26,51 +26,51 @@ class HomeView extends GetView<HomeController> {
       ),
       body: Obx(() => controller.isLoading.value
           ? SkeletonListView()
-          : Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: ListView.builder(
-                itemCount: controller.listNews.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: EdgeInsets.only(top: 50),
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: backgroundColor,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 5,
-                            color: Colors.black54,
-                            offset: Offset(0.0, 0.75)
-                          )
-                        ]),
-                    padding: EdgeInsets.only(
-                        left: 19, right: 8, top: 26, bottom: 16),
-                    child: ListTile(
-                      onTap: () => Get.toNamed(Routes.DETAIL_ARTICLE,
-                          arguments: {
-                            'id': controller.listNews[index].id.toString()
-                          }),
-                      title: Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
-                        child: Text(
-                            controller.listNews[index].title.toString(),
-                            style: headline2),
-                      ),
-                      subtitle: Text(
-                        controller.listNews[index].body.toString(),
-                        style:
-                            headline3.copyWith(fontWeight: FontWeight.w400),
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: false,
-                        textAlign: TextAlign.justify
-                      ),
+          : ListView.builder(
+            itemCount: controller.listNews.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Container(
+                  margin: EdgeInsets.only(top: 50),
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: backgroundColor,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 5,
+                          color: Colors.black54,
+                          offset: Offset(0.0, 0.75)
+                        )
+                      ]),
+                  padding: EdgeInsets.only(
+                      left: 19, right: 8, top: 26, bottom: 16),
+                  child: ListTile(
+                    onTap: () => Get.toNamed(Routes.DETAIL_ARTICLE,
+                        arguments: {
+                          'id': controller.listNews[index].id.toString()
+                        }),
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 15),
+                      child: Text(
+                          controller.listNews[index].title.toString(),
+                          style: headline2),
                     ),
-                  );
-                },
-              ),
-            )),
+                    subtitle: Text(
+                      controller.listNews[index].body.toString(),
+                      style:
+                          headline3.copyWith(fontWeight: FontWeight.w400),
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                      textAlign: TextAlign.justify
+                    ),
+                  ),
+                ),
+              );
+            },
+          )),
       floatingActionButton: FloatingActionButton(
         backgroundColor: backgroundColor,
         onPressed: () => Get.toNamed(Routes.POST_ARTICLE),
